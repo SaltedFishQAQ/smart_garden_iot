1c298863246fee8adaaf80331342caced7267769
frozen <791142033@qq.com>
2024-04-16 13:39:48 +0200

    added base adapter http client logic

--- database/common/base_adapter.py
+++ database/common/base_adapter.py
@@ -1,21 +1,25 @@
 from common.mqtt import MQTTClient
+from common.http_client import HTTPClient
 
 
 class BaseAdapter:
     def __init__(self, adapter_id):
-        self.mqtt_client = None
         self.adapter_id = adapter_id
-        self.broker = "mqtt.eclipseprojects.io"
-        self.port = 1883
-        # TODO:
-        self.routes = {}
+        # mqtt
+        self.mqtt_client = None
+        self.mqtt_broker = None
+        self.mqtt_port = None
+        # http
+        self.http_client = None
+        self.http_host = None
+        self.http_port = None
 
-    def init_mqtt_client(self, broker, port):
+    def init_mqtt_client(self, broker="mqtt.eclipseprojects.io", port=1883):
         if self.mqtt_client is not None:
             return
 
-        self.broker = broker
-        self.port = port
+        self.mqtt_broker = broker
+        self.mqtt_port = port
         self.mqtt_client = MQTTClient(self.adapter_id, broker, port)
         self.mqtt_client.start()
 
@@ -40,9 +44,18 @@ class BaseAdapter:
         self.mqtt_client.publish(topic, message)
         return True, ""
 
-    def init_http_client(self):
-        # TODO: add http client implement
-        print(self.adapter_id)
+    def init_http_client(self, host="localhost", port=8080):
+        if self.http_client is not None:
+            return
 
-    def add_http_route(self, route, handler):
-        self.routes[route] = handler
+        self.http_host = host
+        self.http_port = port
+        self.http_client = HTTPClient(host, port)
+        self.http_client.start()
+
+    def remove_http_client(self):
+        if self.http_client is None:
+            return
+
+        self.http_client.stop()
+        self.http_client = None
