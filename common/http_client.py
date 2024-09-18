e898ce42d0364224f320923a66adec9e1a2be5d9
frozen <791142033@qq.com>
2024-09-18 21:27:19 +0200

    code fix

--- common/http_client.py
+++ common/http_client.py
@@ -110,7 +110,7 @@ class HTTPClient(object):
 
     @final
     def OPTIONS(self, *uri, **params):
-        cherrypy.response.headers['Access-Control-Allow-Origin'] = 'http://localhost:8080'
+        cherrypy.response.headers['Access-Control-Allow-Origin'] = '*'
         cherrypy.response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
         cherrypy.response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
         return ''
