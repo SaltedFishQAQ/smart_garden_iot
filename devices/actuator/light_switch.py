8583411b8d38d8c41420dad663756f8d62a654a9
frozen <791142033@qq.com>
2024-10-16 16:29:11 +0200

    add light logic on kit

--- devices/actuator/light_switch.py
+++ devices/actuator/light_switch.py
@@ -1,12 +1,18 @@
+# import RPi.GPIO as GPIO
 from devices.biz.base_actuator import BaseActuator
 
 
 class LightSwitch(BaseActuator):
     def __init__(self):
         super().__init__("light switch")
+        # GPIO.setmode(GPIO.BCM)
+        # self.pin = 17
+        # GPIO.setup(self.pin, GPIO.OUT)
 
     def _on(self):
+        # GPIO.output(self.pin, GPIO.HIGH)
         return "turn on the light"
 
     def _off(self):
+        # GPIO.output(self.pin, GPIO.LOW)
         return "turn off the light"
