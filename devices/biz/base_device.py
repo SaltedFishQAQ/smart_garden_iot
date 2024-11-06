42c1aae4f6107c413eb354fd13d98fe4709cfd8d
frozen <791142033@qq.com>
2024-11-07 03:28:43 +0800

    code fix

--- devices/biz/base_device.py
+++ devices/biz/base_device.py
@@ -83,7 +83,7 @@ class BaseDevice:
 
     def _handle_command(self, client, userdata, msg):
         content = msg.payload.decode('utf-8')
-        print(content)
+        print(f'_handle_command: {content}')
         data_dict = json.loads(content)
         print(f"device: {self.device_name}, receive command: {data_dict}")
 
