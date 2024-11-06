9874d312d4a6aa8262a4019c75637f3bcc8dac7c
frozen <791142033@qq.com>
2024-11-07 04:17:55 +0800

    added soil device

--- devices/server_main.py
+++ devices/server_main.py
@@ -3,6 +3,7 @@ from devices.light_controller import LightController
 from devices.oxygen_controller import OxygenController
 from devices.hygrometer import Hygrometer
 from devices.thermometer import Thermometer
+from devices.soil_moisture_device import SoilMoistureDevice
 
 
 if __name__ == '__main__':
@@ -14,6 +15,7 @@ if __name__ == '__main__':
     light_control.start()
     oxygen_control = OxygenController('oxygen')
     oxygen_control.start()
+    soil_device = SoilMoistureDevice('soil moisture')
 
     try:
         while True:
@@ -23,3 +25,4 @@ if __name__ == '__main__':
         humidity_sensor.stop()
         light_control.stop()
         oxygen_control.stop()
+        soil_device.stop()
---
+++ devices/soil_moisture_device.py
@@ -0,0 +1,47 @@
+import json
+from devices.biz.base_device import BaseDevice
+from devices.sensor.soil_moisture import SoilMoistureSensor
+
+
+class SoilMoistureDevice(BaseDevice):
+    def __init__(self, name):
+        super().__init__(name)
+        self.sensor = SoilMoistureSensor("clay")
+        self.sensor.receiver = self.handle_data
+
+    def start(self):
+        super().start()
+        self.sensor.start()
+
+    def stop(self):
+        super().stop()
+        self.sensor.stop()
+
+    def handle_working(self, status):
+        if status:
+            self.sensor.start()
+        else:
+            self.sensor.stop()
+
+    def handle_opt(self, opt, status):
+        if self.sensor.running == status:
+            return
+
+        if status:
+            self.sensor.start()
+        else:
+            self.sensor.stop()
+
+    def handle_data(self, data_str):
+        data = json.loads(data_str)
+        if 'value' not in data:
+            print("data missing humidity value, data: {}".format(data))
+            return
+        print(f"record data: {self.device_name}, {data}")
+        self.record_data(data)
+
+    def status(self):
+        return {
+            'device': self.working,
+            'sensor': self.sensor.running
+        }
