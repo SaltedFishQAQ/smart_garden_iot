1ace3cf079fc6e80ae5ce467b1d0e6107e390da0
david <s325715@studenti.polito.it>
2024-11-07 13:34:03 +0100

    fetch area data

--- service/decision/watering_coordinator.py
+++ service/decision/watering_coordinator.py
@@ -1,41 +1,50 @@
 import time
 import subprocess
 import constants.const as const
+#from service.user.logic.area import Logic, Delegate  # Import Logic class and its dependencies
+from mysqptest import Logic, Delegate  # Import Logic class and its dependencies
 
-# Define the dictionary of areas and their soil types
-area_soil_map = {
-    'area1': 'Clay',
-    'area2': 'Sandy',
-    'area3': 'Loamy',
-    'area4': 'Silty'
-}
 
-# Define the interval (5 minutes in seconds)
+#MYSQL
+delegate = Delegate()
+logic = Logic(delegate)
+
 CHECK_INTERVAL = const.CHECK_INTERVAL
 RUNNING_CHECK_INTERVAL = const.RUNNING_CHECK_INTERVAL
 
-# Dictionary to track running processes for each area
 processes = {}
 
+
+def fetch_area_soil_map():
+    # Fetch area list from MySQL
+    response = logic.list(params={})
+
+    if response['code'] == 0:
+        # Create a dictionary mapping area name to its soil type
+        area_soil_map = {area['name']: area['soil_type'].capitalize() for area in response['list']}
+        return area_soil_map
+    else:
+        print("Error fetching area data:", response['message'])
+        return {}
+
+
 while True:
+    # check for updated area
+    area_soil_map = fetch_area_soil_map()
+
     for area, soil_type in area_soil_map.items():
         # Check if there's an active process for this area
         if area in processes:
             if processes[area].poll() is None:
-                ##print(f"Process for {area} is still running. Skipping new command.")
+                # Process is still running; skip starting a new one
                 time.sleep(RUNNING_CHECK_INTERVAL)
                 continue
             else:
-                # If the process has finished, we can remove it from the tracking dictionary
-                ##print(f"Process for {area} has finished.")
+                # If the process has finished, remove it from the tracking dictionary
                 processes.pop(area)
 
-        # Start a new process for the area if no active process exists
-        ##print(f"Sending request to watering_decision.py for {area} with soil type {soil_type}")
+        # New process starts as the last one is done
         process = subprocess.Popen(['python', 'watering_decision.py', area, soil_type])
-        processes[area] = process  # Store the process in the dictionary
+        processes[area] = process
 
-
-    # Wait for the specified interval before the next check
-        ##print(f"Sleeping for {CHECK_INTERVAL / 60} minutes before the next check...")
-        time.sleep(CHECK_INTERVAL)
+    time.sleep(CHECK_INTERVAL)
