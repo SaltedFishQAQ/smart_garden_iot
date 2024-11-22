9d9fe6623d03864229367f80dd79333fdfd086f1
frozen <791142033@qq.com>
2024-11-23 02:46:23 +0800

    code fix

--- service/user/logic/operation.py
+++ service/user/logic/operation.py
@@ -1,18 +1,19 @@
 import requests
 import constants.http as const_h
 from http import HTTPMethod
+from service.user.logic.base import Common
 
 
-class Logic:
+class Logic(Common):
     def __init__(self, delegate):
-        self.delegate = delegate
-        self.mysql_base_url = f'{const_h.MYSQL_HOST}:{const_h.SERVICE_PORT_MYSQL}'
+        super().__init__(delegate)
         self.influx_base_url = f'{const_h.INFLUX_HOST}:{const_h.SERVICE_PORT_INFLUX}'
 
     def register_handler(self):
         self.delegate.http_client.add_route(const_h.USER_OPERATION_GET, HTTPMethod.GET, self.list)
 
     def list(self, params):
+        self.match_area_names(params)
         resp = requests.get(self.influx_base_url + const_h.INFLUX_OPERATION_GET, params)
 
         result = []
