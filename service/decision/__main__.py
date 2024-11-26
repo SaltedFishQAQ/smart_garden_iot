652921e24e9c5a6d19eb72590a71f44057d4f6fe
frozen <791142033@qq.com>
2024-11-27 05:40:30 +0800

    code fix

--- service/decision/__main__.py
+++ service/decision/__main__.py
@@ -4,8 +4,6 @@ from service.decision.decision_service import DecisionService
 if __name__ == '__main__':
     s = DecisionService()
     s.start()
-    s.register_controller()
-    s.run()
 
     try:
         while True:
